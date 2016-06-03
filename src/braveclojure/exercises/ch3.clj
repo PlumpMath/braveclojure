dacb05d4aa6d53f8d0c0d2543a4666f392e718c7
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-06-03 16:55:53 +0800

    autofill the problem on ch3
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- src/braveclojure/exercises/ch3.clj
+++ src/braveclojure/exercises/ch3.clj
@@ -2,7 +2,13 @@
 
 ;; http://www.braveclojure.com/do-things/
 
-;; These exercises are meant to be a fun way to test your Clojure knowledge and to learn more Clojure functions. The first three can be completed using only the information presented in this chapter, but the last three will require you to use functions that haven’t been covered so far. Tackle the last three if you’re really itching to write more code and explore Clojure’s standard library. If you find the exercises too difficult, revisit them after reading Chapters 4 and 5—you’ll find them much easier.
+;; These exercises are meant to be a fun way to test your Clojure knowledge and
+;; to learn more Clojure functions. The first three can be completed using only
+;; the information presented in this chapter, but the last three will require
+;; you to use functions that haven’t been covered so far. Tackle the last three
+;; if you’re really itching to write more code and explore Clojure’s standard
+;; library. If you find the exercises too difficult, revisit them after reading
+;; Chapters 4 and 5—you’ll find them much easier.
 
 ;; ------------------------------------------------------------
 ;; 1. Use the str, vector, list, hash-map, and hash-set functions.
@@ -19,7 +25,8 @@
 (add-100 1)                             ; => 101
 
 ;; ------------------------------------------------------------
-;; 3. Write a function, dec-maker, that works exactly like the function inc-maker except with subtraction:
+;; 3. Write a function, dec-maker, that works exactly like the function
+;; inc-maker except with subtraction:
 ;; (def dec9 (dec-maker 9))
 ;; (dec9 10)
 ;; ; => 1
@@ -45,7 +52,9 @@
 (mapset inc [1 1 2 2])                  ; => #{2 3}
 
 ;; ------------------------------------------------------------
-;; 5. Create a function that’s similar to symmetrize-body-parts except that it has to work with weird space aliens with radial symmetry. Instead of two eyes, arms, legs, and so on, they have five.
+;; 5. Create a function that’s similar to symmetrize-body-parts except that it
+;; has to work with weird space aliens with radial symmetry. Instead of two
+;; eyes, arms, legs, and so on, they have five.
 
 (def body-part [{:name "head"   :size 10}
                 {:name "eyes-1" :size 1}
@@ -62,7 +71,12 @@
           [] asym-body-parts))
 
 ;; ------------------------------------------------------------
-;; 6. Create a function that generalizes symmetrize-body-parts and the function you created in Exercise 5. The new function should take a collection of body parts and the number of matching body parts to add. If you’re completely new to Lisp languages and functional programming, it probably won’t be obvious how to do this. If you get stuck, just move on to the next chapter and revisit the problem later.
+;; 6. Create a function that generalizes symmetrize-body-parts and the function
+;; you created in Exercise 5. The new function should take a collection of body
+;; parts and the number of matching body parts to add. If you’re completely new
+;; to Lisp languages and functional programming, it probably won’t be obvious
+;; how to do this. If you get stuck, just move on to the next chapter and
+;; revisit the problem later.
 
 (defn matching-part
   [part i]
