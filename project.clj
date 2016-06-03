442b9d0a732feaab042ffa6f56268f04b5d619d7
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-06-03 14:46:18 +0800

    fix project url
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject braveclojure "0.1.0-SNAPSHOT"
   :description "\"Clojure for the brave and true\" learning notes and exercises."
-  :url "https://github.com/coldnew/braveclojure-note"
+  :url "https://github.com/coldnew/braveclojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha4"]])
