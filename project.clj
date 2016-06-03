b526f05c63cec076c1f62482424805defab14a20
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-06-03 10:56:38 +0800

    use clojure 1.9.0-alpha4
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject braveclojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "\"Clojure for the brave and true\" learning notes and exercises."
+  :url "https://github.com/coldnew/braveclojure-note"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha4"]])
